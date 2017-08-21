f209134eca6a8def7eba59c8b8d229981bf962b1
Fabien Charlé <fabien.charle@student.hpi.de>
2017-08-21 12:34:56 +0200

    add ErrorHandler

---
+++ wikipedia/errorhandler.js
@@ -0,0 +1,47 @@
+
+
+function getErrorMessage(status){
+    var msg = {};
+
+    msg.status = String(status);
+    switch (status){
+        case 400 :
+            msg.title = "Bad Request";
+            msg.detail = "invalid parameter";
+            break;
+        case 500 :
+            msg.title = "Internal Server Error";
+            msg.detail =  "Something went wrong.";
+            break;
+        case 406 :
+            msg.title = "Not Acceptable";
+            msg.detail =  "Content type is not Acceptable";
+            break;
+        case 404 :
+        default :
+            msg.title = "Not Found";
+            msg.detail =  "The requested resource could not be found.";
+            break;
+    }
+    return msg;
+}
+
+
+modules.exports.getMessage = function (status) {
+    var message = {
+        "jsonapi":{
+            "version": "1.0",
+            "meta" :{
+                "name": "wikipedia-content-adapter",
+                "source": "https://github.com/schul-cloud/wikipedia-content-adapter",
+                "description": "This is an adpter for infos from wikipedia."
+            }
+        },
+        "errors": []
+    };
+
+    message.errors.push(getErrorMessage(status));
+    return message;
+
+
+}
