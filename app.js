364bcb68386889ec3cd2234ae73a42f52934c2f4
Fabien Charlé <fabien.charle@student.hpi.de>
2017-08-21 11:38:01 +0200

    Add Error Callback

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/v1', function (req, res) {
+    res.contentType("application/+vm.json");
+    var wikipedia = require("wikipedia/request.js").makeRequest(req.query,
+        function (message,status) // error Message callback
+        {
+            res.status(status);
+            res.send(message);
+        },
+        function (message) // sendMessage callback
+        {
+            res.send(message);
+        }
+    );
 });
 
+
+
+process.title = "wikipedia";
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
